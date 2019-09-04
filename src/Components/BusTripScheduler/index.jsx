7f5bf22645a7fccee0d202349df9def1b9ed577d
kkch3n <kevin.kang.chen@gmail.com>
2019-09-04 02:54:54 -0700

    WIP drag and drop

--- src/Components/BusTripScheduler/index.jsx
+++ src/Components/BusTripScheduler/index.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import withBusTripManager from './with-bus-trip-manager'
 import Trip from '../Trip'
 
-const BusTripScheduler = ({ trips, selectTrip }) => (
-    <div
-        style={{ marginTop: '1rem', backgroundColor: 'gray', padding: '1rem' }}
-    >
-        BusTripScheduler
-        {trips.map((trip, i) => {
-            return <Trip key={i} {...trip} selectTrip={() => selectTrip(i)} />
-        })}
-    </div>
-)
+class BusTripScheduler extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.onDrop = this.onDrop.bind(this)
+        this.onDragOver = this.onDragOver.bind(this)
+    }
+
+    onDragOver = (event) => {
+        event.preventDefault();
+    }
+
+    onDrop = (event, busId) => {
+        console.log('onDrop on div: ', busId);
+        event.dataTransfer.setData("busId", busId);
+    }
+
+    render() {
+        const { trips, selectTrip } = this.props
+        return (
+            <div
+                style={{ marginTop: '1rem', backgroundColor: 'gray', padding: '1rem' }}
+            >
+                BusTripScheduler
+            {trips.map((trip, i) => {
+                    return <div
+                        key={i}
+                        onDragOver={(event) => this.onDragOver(event)}
+                        onDrop={(event) => { this.onDrop(event, i) }}
+                        style={{ margin: '0.5rem', backgroundColor: 'yellow', padding: '0.5rem', borderBottom: '1px solid black' }}
+                    >
+                        <Trip {...trip} selectTrip={() => selectTrip(i)} />
+                    </div>
+                })}
+            </div>
+        )
+    }
+}
 
 export default withBusTripManager(BusTripScheduler)
--- src/Components/Trip/index.jsx
+++ src/Components/Trip/index.jsx
@@ -1,21 +1,34 @@
 import React, {PureComponent} from 'react'
 
-const Trip = ({ id, startTime, endTime, selected, selectTrip }) => (
-    <div
-        style={{ margin: '0.5rem', backgroundColor: 'yellow', padding: '0.5rem', borderBottom: '1px solid black' }}
-    >
-        <div
-            style={{
-                marginLeft: `${startTime}px`,
-                width: `${endTime - startTime}px`,
-                backgroundColor: selected ? 'gray' : '',
-                border: '1px solid black'
-            }}
-            onClick={selectTrip}
-        >
-            {id}
-        </div>
-    </div>
-)
+class Trip extends PureComponent {
+    constructor(props) {
+        super(props)
+        this.onDragStart = this.onDragStart.bind(this)
+    }
 
+    onDragStart = (event, trip) => {
+        console.log('dragstart on div: ', trip);
+        event.dataTransfer.setData("trip", trip);
+    }
+
+    render() {
+        const { id, startTime, endTime, selected, selectTrip } = this.props
+        return (            
+            <div
+                draggable
+                onDragStart={(event) => this.onDragStart(event, id)}
+                style={{
+                    marginLeft: `${startTime}px`,
+                    width: `${endTime - startTime}px`,
+                    backgroundColor: selected ? 'gray' : '',
+                    border: '1px solid black'
+                }}
+                onClick={selectTrip}
+            >
+                {id}
+            </div>
+        )
+
+    }
+} 
 export default Trip
