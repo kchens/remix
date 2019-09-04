4ad3b56cbc1ccd23eddb840b60b928d6721aa86a
kkch3n <kevin.kang.chen@gmail.com>
2019-09-03 21:34:50 -0700

    Adds toggleSelect color when selecting Trip

--- src/Components/BusTripScheduler/index.jsx
+++ src/Components/BusTripScheduler/index.jsx
@@ -1,5 +1,6 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import withBusTripManager from './with-bus-trip-manager'
+import Trip from '../Trip'
 
 const BusTripScheduler = ({trips}) => (
     <div
@@ -7,22 +8,7 @@ const BusTripScheduler = ({trips}) => (
     >
         BusTripScheduler
         {trips.map((trip, i) => {
-            const left = `${trip.startTime}px`
-            const right = `${trip.endTime}px`
-            console.log(left)
-            console.log(right)
-            return (
-                <div
-                    key={i}
-                    style={{ margin: '0.5rem', backgroundColor: 'yellow', padding: '0.5rem', borderBottom: '1px solid black' }}
-                >
-                    <div
-                        style={{ marginLeft: left, width: right, backgroundColor: 'orange', padding: '0.5rem', border: '1px solid black' }}
-                    >
-                        {trip.id}
-                    </div>
-                </div>
-            )
+            return <Trip key={i} {...trip} />
         })}
     </div>
 )
--- src/Components/Trip/index.jsx
+++ src/Components/Trip/index.jsx
@@ -1,1 +1,32 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
+
+class Trip extends PureComponent {
+    toggleSelectColor(e) {
+        const trip = e.target
+        const color = trip.style.backgroundColor ? '' : 'gray'
+        trip.style.backgroundColor = color
+    }
+
+    render() {
+        const { id, startTime, endTime } = this.props
+        return (
+            <div
+                style={{ margin: '0.5rem', backgroundColor: 'yellow', padding: '0.5rem', borderBottom: '1px solid black' }}
+            >
+                <div
+                    style={{
+                        marginLeft: `${startTime}px`,
+                        width: `${endTime}px`,
+                        padding: '0.5rem',
+                        border: '1px solid black'
+                    }}
+                    onClick={this.toggleSelectColor}
+                >
+                    {id}
+                </div>
+            </div>
+        )
+    }
+} 
+
+export default Trip
